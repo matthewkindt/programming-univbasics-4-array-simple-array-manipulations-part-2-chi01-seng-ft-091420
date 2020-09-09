878eb9e49060cb7b680fad8bb21a8a3b22ac04b0
Matthew Kindt <matthewkindt@yahoo.com>
2020-09-09 20:31:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,3 @@
 def using_concat(my_favorite_things, all_my_favs)
-  my_favorite_things = ["raindrops on roses", "whiskers on kittens"].concat( ["sports cars", 
-   "flatiron school"] )
-   binding.pry
+  my_favorite_things.concat(all_my_favs)
 end
