960c8f4f210513084f050fb6c9606d553f012afd
Matthew Kindt <matthewkindt@yahoo.com>
2020-09-09 20:39:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,5 +3,5 @@ def using_concat(my_favorite_things, all_my_favs)
 end
 
 def using_insert(list_of_programing_languages, another_language = "Python" new_array)
-list_of_programing_languages.insert(4, another_language)
+list_of_programing_languages.insert(4, "Python")
 end
