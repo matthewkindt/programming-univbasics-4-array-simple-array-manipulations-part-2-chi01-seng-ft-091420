79681ac66651e8c0507d0c00d21dc55e4b6bfd56
Matthew Kindt <matthewkindt@yahoo.com>
2020-09-09 20:50:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,5 +15,5 @@ def using_flatten(instruments)
 end
 
 def using_delete(no_offense_steven, instructors)
-no_offense_steven = instructors.delete "Steven"
+instructors.delete "no_offense_steven"
 end
